bbe6a81ac6c5408dfda7bded05427d4ec00b2762
Yuri Kaspervich <ykas.gg@gmail.com>
2016-11-09 18:24:35 +0300

    Size of interpreter memory is less than 30000 cells

--- brainfuck.rb
+++ brainfuck.rb
@@ -1,7 +1,11 @@
 require "minitest/autorun"
 
 class BrainfuckTest < Minitest::Test
-  def test_it_works
-    assert true
+  def test_size_of_brainfuck_interpreter_memory_is_less_than_30000_cells
+    assert_equal Brainfuck::MEMORY_SIZE, 30_000
   end
+end
+
+class Brainfuck
+  MEMORY_SIZE = 30_000
 end
