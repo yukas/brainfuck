aa4e2669f53ed8f407eee8802182f7897fc86d7c
Yuri Kaspervich <ykas.gg@gmail.com>
2016-11-15 15:48:44 +0300

    Minus sign (-) decrements current cell value

--- brainfuck.rb
+++ brainfuck.rb
@@ -13,7 +13,7 @@ class BrainfuckTest < Minitest::Test
     assert_equal 0, subject.cell_value_at(29_999) 
   end
   
-  def test_pointer_is_point_to_beginning_of_memory
+  def test_pointer_points_to_beginning_of_memory
     assert_equal 0, subject.current_cell_index
   end
   
@@ -37,6 +37,15 @@ class BrainfuckTest < Minitest::Test
     
     assert_equal 1, subject.current_cell_value
   end
+  
+  def test_minus_sign_decrements_current_cell_value
+    subject.set_current_cell_value(5)
+    
+    subject.code = "-"
+    subject.execute_code
+    
+    assert_equal 4, subject.current_cell_value
+  end
 end
 
 class Brainfuck
@@ -59,6 +68,8 @@ class Brainfuck
         @current_cell_index -= 1
       when "+"
         set_cell_value_at(current_cell_index, cell_value_at(current_cell_index) + 1)
+      when "-"
+        set_current_cell_value(current_cell_value - 1)
       end
     end
   end
@@ -73,6 +84,10 @@ class Brainfuck
   
   def current_cell_value
     cell_value_at(current_cell_index)
+  end
+  
+  def set_current_cell_value(value)
+    set_cell_value_at(current_cell_index, value)
   end
   
   private
