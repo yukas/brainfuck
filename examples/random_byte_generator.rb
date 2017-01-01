dab8ac4a176e35a08ab454c572ecc0d135bfd283
Yuri Kaspervich <ykas.gg@gmail.com>
2017-01-02 00:35:40 +0300

    Add Towers of Hanoi and Random byte generator examples

---
+++ examples/random_byte_generator.rb
@@ -0,0 +1,23 @@
+require_relative '../brainfuck'
+require_relative '../ascii_encoding'
+
+brainfuck = Brainfuck.new(STDIN, STDOUT, ASCIIEncoding.new)
+
+brainfuck.execute_code(
+<<EOF
+>>>++[
+    <++++++++[
+        <[<++>-]>>[>>]+>>+[
+            -[->>+<<<[<[<<]<+>]>[>[>>]]]
+            <[>>[-]]>[>[-<<]>[<+<]]+<<
+        ]<[>+<-]>>-
+    ]<.[-]>>
+]
+"Random" byte generator using the Rule 30 automaton.
+Doesn't terminate; you will have to kill it.
+To get x bytes you need 32x+4 cells.
+Turn off any newline translation!
+Daniel B Cristofani (cristofdathevanetdotcom)
+http://www.hevanet.com/cristofd/brainfuck/
+EOF
+)
